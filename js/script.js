497cc09c190f7a114c5cd594c99ebc0393ab2a78
Mayar Arabi <mayararaby246@gmail.com>
2020-11-24 10:08:07 -0800

    function for check cards added in JS

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Effect Duration
+let duration = 1000;
+// Select Blocks Container
+let blocksContainer = document.querySelector(".container-game-blocks");
+// Create Array From Game Blocks
+let blocks = Array.from(blocksContainer.children);
+
+
+// Check Matched Block
+function checkMatchedBlocks(firstBlock, secondBlock) {
+
+    let triesElement = document.querySelector('.tries span');
+
+    if (firstBlock.dataset.tech === secondBlock.dataset.tech) {
+
+        firstBlock.classList.remove('flipped');
+        secondBlock.classList.remove('flipped');
+
+        firstBlock.classList.add('mached');
+        secondBlock.classList.add('mached');
+
+        document.getElementById('success').play();
+
+    } else {
+
+        triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+
+        setTimeout(() => {
+
+            firstBlock.classList.remove('flipped');
+            secondBlock.classList.remove('flipped');
+
+        }, duration);
+
+        document.getElementById('fail').play();
+
+    }
+
+}
