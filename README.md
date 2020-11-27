b2b8d694658a8befa8aa141d5283f9d8fe12cf64
Ali khaled <ka99ali@gmail.com>
2020-11-27 19:19:04 +0200

    add all pages in one page

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Memory_game01
+
+This Game is built uopn the JS language in help with local storage.
+
+
+
+
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,77 +1,129 @@
-let duration = 1000;
-let blocksContainer = document.querySelector(".container-game-blocks");
-let blocks = Array.from(blocksContainer.children);
-
-let orderRange = Array.from(Array(blocks.length).keys());
+let signup_form = document.querySelector('.control-button .sign_up')
+let signin_form = document.querySelector('.control-button .sign_in')
+let go_back = document.querySelector('.go_back')
  
-function shuffle(arra1) {
-    var ctr = arra1.length, temp, index;
-    while (ctr > 0) {
-        index = Math.floor(Math.random() * ctr);
-        ctr--;
-        temp = arra1[ctr];
-        arra1[ctr] = arra1[index];
-        arra1[index] = temp;
-    }
-    return arra1;
+function goBack() {
+    window.location.reload();
 }
 
-shuffle(orderRange)
+document.querySelector('.control-button #signup_btn').onclick = function () {
+    signup_form.style.display = "block";
+    signin_form.style.display = "none";
+    this.style.display = "none";
+    document.querySelector('.control-button #signin_btn').style.display = "none"
+}
 
+document.querySelector('.control-button #signin_btn').onclick = function () {
+    signin_form.style.display = "block";
+    signup_form.style.display = "none";
+    this.style.display = "none";
+    document.querySelector('.control-button #signup_btn').style.display = "none";
+}
+var Uname = document.getElementById('Uname');
+var pw = document.getElementById('pw');
+
+function store_user() {
+    if(Uname.value == '' || Uname.value == null || pw.value == '' || pw.value == null ) {
+        alert('username or password is Empty!');
+    }else {
+        localStorage.setItem('name', Uname.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Good, account created.. try login agian!');
+    }
+}
+
+function check_user() {
+
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    if(userName.value !== storedName || userPw.value !== storedPw) {
+        alert('username or password not correct!');
+    }else {
+        document.querySelector('.control-button').remove();
+        document.querySelector('.info .hi_name span').innerHTML = userName.value;
+    }
+}
+///////////////////////////////////////////////////////
+let duration = 1000;
+let blocksContainer = document.querySelector('.container-game-blocks'),
+    blocks = Array.from(blocksContainer.children),
+    orderRange = [...Array(blocks.length).keys()];
+function shuffle(array) {
+    let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+shuffle(orderRange);
 blocks.forEach((block, index) => {
     block.style.order = orderRange[index];
     block.addEventListener('click', function () {
-        Flip(block);
+        flipping(block);
     });
-
 });
-
-function Flip(selectBlock) {
+function flipping(selectBlock) {
     selectBlock.classList.add('flipped');
-
-    let Flippeing = blocks.filter(Flipped => Flipped.classList.contains('flipped'));
-    if (Flippeing.length === 2) {
-        Stop();
-        checkMatchedBlocks(Flippeing[0], Flippeing[1]);
+    let allFlippedBlock = blocks.filter(filippedBlock => filippedBlock.classList.contains('flipped'));
+    if (allFlippedBlock.length === 2) {
+        stopClick();
+        cheak(allFlippedBlock[0], allFlippedBlock[1]);
     }
 }
-
-function Stop() {
+function stopClick() {
     blocksContainer.classList.add('no-click');
     setTimeout(() => {
         blocksContainer.classList.remove('no-click');
     }, duration);
 }
-
-
-function checkMatchedBlocks(firstBlock, secondBlock) {
-
-    let triesElement = document.querySelector('.tries span');
-
-    if (firstBlock.dataset.tech === secondBlock.dataset.tech) {
-
+function cheak(firstBlock, secondBlock) {
+    let tries = document.querySelector('.tries span');
+    if (firstBlock.getAttribute('data-tech') === secondBlock.getAttribute('data-tech')) {
         firstBlock.classList.remove('flipped');
         secondBlock.classList.remove('flipped');
-
         firstBlock.classList.add('mached');
         secondBlock.classList.add('mached');
-
         document.getElementById('success').play();
-
     } else {
-
-        triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
-
+        tries.innerHTML = parseInt(tries.innerHTML) + 1;
         setTimeout(() => {
-
             firstBlock.classList.remove('flipped');
             secondBlock.classList.remove('flipped');
-
         }, duration);
-
         document.getElementById('fail').play();
-
     }
-
+}
+function checkMatchedBlocks(firstBlock, secondBlock) {
+    let triesElement = document.querySelector('.tries span');
+    if (firstBlock.dataset.tech === secondBlock.dataset.tech) {
+        firstBlock.classList.remove('flipped');
+        secondBlock.classList.remove('flipped');
+        firstBlock.classList.add('mached');
+        secondBlock.classList.add('mached');
+        document.getElementById('success').play();
+    } else {
+        triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+        setTimeout(() => {
+            firstBlock.classList.remove('flipped');
+            secondBlock.classList.remove('flipped');
+        }, duration);
+    }
 }
 
+document.querySelector(".restart span").onclick = function(){
+    shuffle(orderRange);
+    blocks.forEach((block, index) => {
+        block.classList.remove('mached');
+        block.style.order = orderRange[index];
+        document.querySelector('.tries span').innerHTML = 0;
+    });
+}
